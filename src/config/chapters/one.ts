e5cf726dfdcb1ccd22b21eae976da19eded5feb4
coll2112 <lgc9312@gmail.com>
2023-02-18 21:18:53 -0600

    edited some styles and story

--- src/config/chapters/one.ts
+++ src/config/chapters/one.ts
@@ -1,40 +1,34 @@
 import { Chapter, ChapterChoices } from '~/types/story'
 
-export const choices: ChapterChoices = {
+const CHOICES: ChapterChoices = {
   startChapter: 'start-chapter',
-  readyToPlay: 'ready-to-play',
-  letsPlay: 'lets-play',
-  nah: 'nah',
+  lineTwo: 'line-one',
+  drinkCoffee: 'drink-coffee',
+  ignoreCoffee: 'ignore-coffee',
   endChapter: 'end-chapter'
 }
 
 export const ChapterOne: Chapter = {
   chapterIndex: 0,
-  chapterName: 'Chapter 1 - The Adventure Begins...',
+  chapterName: 'Chapter 1: The Long Dark',
   sections: {
-    [choices.startChapter]: {
-      text: 'Hi, welcome to the game',
+    [CHOICES.startChapter]: {
+      text: 'The sky is the color of ash. The wind hits the old window of your home, rattling it as if someone just launched a cannon right next to you. The cold draft seeps underneath the doorway.',
+      choices: [{ text: "It's so cold...", event: CHOICES.lineTwo }]
+    },
+    [CHOICES.lineTwo]: {
+      text: "It doesn't take long for air to hit your feet, the only thing your blanket isn't long enough to reach. The morning coffee you made sits in front of you still untouched.",
       choices: [
-        {
-          event: choices.readyToPlay,
-          text: 'Thanks!'
-        }
+        { text: 'Take a sip of coffee', event: CHOICES.drinkCoffee },
+        { text: 'Ignore the coffee', event: CHOICES.ignoreCoffee }
       ]
     },
-    [choices.readyToPlay]: {
-      text: 'Are you ready to play?',
-      choices: [
-        { event: choices.letsPlay, text: "Let's do it!" },
-        { event: choices.nah, text: "I don't wanna" }
-      ]
+    [CHOICES.ignoreCoffee]: {
+      text: 'The cup of coffee continues to sit untouched, getting colder every second that goes by. Maybe you should drink it...?',
+      choices: [{ text: 'Take a sip of coffee', event: CHOICES.drinkCoffee }]
     },
-    [choices.letsPlay]: {
-      text: "Then let's begin...",
-      choices: [{ text: 'Okay!!', event: choices.endChapter }]
-    },
-    [choices.nah]: {
-      text: "Oh, that's too bad. Maybe next time.",
-      choices: [{ event: choices.endChapter, text: 'Next' }]
+    [CHOICES.drinkCoffee]: {
+      text: 'The hot coffee hits your lip. You flinch to the sudden, bitter, bite it leaves on your tongue. Perhaps you should have let it cool off more...'
     }
   }
 }
