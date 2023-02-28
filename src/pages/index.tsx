baf1400820c964c4f406692c60ba5151573c82e4
coll2112 <lgc9312@gmail.com>
2023-02-28 13:06:40 -0600

    cleanup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,5 @@
-import Head from 'next/head'
 import TitleScreen from '~components/TitleScreen'
 
-const Home = () => (
-  <>
-    <Head>
-      <link href="https://fonts.googleapis.com" rel="preconnect" />
-      <link href="https://fonts.gstatic.com" rel="preconnect" />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Press+Start+2P&family=VT323&display=swap"
-        rel="stylesheet"
-      />
-    </Head>
-    <TitleScreen />
-  </>
-)
+const Home = () => <TitleScreen />
 
 export default Home
