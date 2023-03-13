ca88376162dacfc1e0b80670137ced03bb3aed74
coll2112 <lgc9312@gmail.com>
2023-03-13 11:57:42 -0500

    created overlay for options

--- src/components/TopBar/index.tsx
+++ src/components/TopBar/index.tsx
@@ -1,3 +1,5 @@
+import clsx from 'clsx'
+import { useState } from 'react'
 import useGameSave from '~/hooks/useGameSave'
 import Button from '~components/Base/Button'
 import ChapterHeading from '~components/ChapterHeading'
@@ -6,16 +8,27 @@ import styles from './topBar.module.scss'
 
 interface Props {
   chapterHeading: string
+  chapterMusic: string
 }
 
-const TopBar = ({ chapterHeading }: Props) => {
+const TopBar = ({ chapterHeading, chapterMusic }: Props) => {
   const { saveData, handleSaveGame, handleLoadGame } = useGameSave()
+  const [isAudioPlaying, setIsAudioPlaying] = useState(false)
+  const [showOptionsOverlay, setShowOptionsOverlay] = useState(false)
+
+  const handleToggleAudio = (): void => {
+    setIsAudioPlaying(!isAudioPlaying)
+  }
+
+  const handleOptionsOverlay = (): void => {
+    setShowOptionsOverlay(!showOptionsOverlay)
+  }
 
   return (
     <div className={styles.container}>
       <ChapterHeading chapterHeading={chapterHeading} />
       {/* eslint-disable-next-line jsx-a11y/media-has-caption */}
-      {/* <audio autoPlay controls loop src={chapterMusic} /> */}
+      <audio controls loop src={chapterMusic} />
       <div className={styles['container-buttons']}>
         <Button type="button" onClick={handleSaveGame}>
           Save Game
@@ -27,6 +40,24 @@ const TopBar = ({ chapterHeading }: Props) => {
         >
           Load Game
         </Button>
+        <Button type="button" onClick={handleOptionsOverlay}>
+          Options
+        </Button>
+      </div>
+      <div
+        className={clsx(
+          styles['options-overlay'],
+          !showOptionsOverlay && styles['options-overlay-hide']
+        )}
+      >
+        <div className={styles['options-overlay-content']}>
+          <div className={styles.option}>
+            <Button type="button" onClick={handleToggleAudio}>
+              Toggle Audio
+            </Button>
+            <p>{isAudioPlaying ? 'On' : 'Off'}</p>
+          </div>
+        </div>
       </div>
     </div>
   )
